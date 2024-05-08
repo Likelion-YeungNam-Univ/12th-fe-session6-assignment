9e2e9b2130acf9e6b3e90ff541dc12410495349e
immms <csi9926@gmail.com>
2024-05-08 16:22:21 +0900

    design: shorts 이미지 추가

--- myapp/src/components/ShortsDetail.js
+++ myapp/src/components/ShortsDetail.js
@@ -3,33 +3,41 @@ const ShortsBody=styled.div`
     width: 100%;
 `
 const Shorts=styled.div`
-    background-color: gray;
-    height: 86%;
     width: 96%;
     border-radius: 12px;
     margin: auto;
 `
+
 const ShortsInfo=styled.div`
+    height: 16%;
     width: 80%;
-    margin: auto;
+    /* margin: 4px auto 0; */
+    margin:auto;
 `
 const Title=styled.div`
     font-size: 10px;
     font-weight: bold;
     color: white;
-    margin: 3px 0;
-
+    /* margin: 3px 0; */
 `
 const Views=styled.div`
     font-size: 10px;
     color: #4c4c4c;
     font-weight: bold;
 `
+const ShortsImage=styled.img`
+    display: block;
+    width: 100%;
+    height: auto;
+    border-radius: 12px;
+`
 
-function ShortsDetail({title, view}){
+function ShortsDetail({short, title, view}){
     return(
         <ShortsBody>
-            <Shorts></Shorts>
+            <Shorts>
+                <ShortsImage src={short}/>
+            </Shorts>
             <ShortsInfo>
                 <Title>{title}</Title>
                 <Views>조회수 {view}회</Views>
